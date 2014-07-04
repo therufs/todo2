f02b2d271e33f34911caf3dc565d3888e68ffd8b
Ruthan <rcfreese@gmail.com>
2014-07-04 00:16:42 -0400

    Write User and Session resources

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'  ## why not redirect_to?
+    end
+  end
+
+  def index
+  end
+
+  # def destroy  ## require a password for this
+  # end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+end
